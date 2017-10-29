d3c013034cde084d0173d7f4764c2aa5ab7f69fa
Pascal <pas05cal@hotmail.com>
2017-10-29 17:55:37 +0100

    Created functions for all the images

--- WebContent/js/work.js
+++ WebContent/js/work.js
@@ -2,6 +2,7 @@ var idSmall = 0;
 var idBig = 0;
 var AreaLeftId = 0;
 var AreaRightId = 0;
+var AreaBottomRightId = 0;
 
 function addSmallImage() {
 	var newContent = ""
@@ -15,10 +16,10 @@ function addSmallImage() {
 			+ "</div>"
 			+ "</div>"
 			+ "<div class='col-md-6'>"
-			+ "<div class='thumbnail id='leftSide" + AreaRightId + "'>"
+			+ "<div class='thumbnail id='rightSide" + AreaRightId + "'>"
 			+ "<button class='test' onclick='addTextArea('leftSide'" + AreaRightId + ")'>Add text area</button><br>"
-			+ "<input name='file" + AreaRightId + "' id='file" + AreaRightId + "' type='file' class='inputfile' onchange='loadFileRight(event, " + AreaRightId + ")'>"
-			+ "<label for='file" + AreaRightId + "'>Add file</label>"
+			+ "<input name='fileRight" + AreaRightId + "' id='fileRight" + AreaRightId + "' type='file' class='inputfile' onchange='loadFileRight(event, " + AreaRightId + ")'>"
+			+ "<label for='fileRight" + AreaRightId + "'>Add file</label>"
 			+ "<img style='height:60%' class='workimage' id='outputRight" + AreaRightId + "'/>"
 			+ "</div>"
 			+ "</div>"
@@ -27,9 +28,6 @@ function addSmallImage() {
 			+ "</div>"
 			+ "</div>"
 			+ "<div id='placeholder'></div>";
-	 
-	
-	
 	
 	var oldContent = document.getElementById("placeholder");
 	oldContent.outerHTML = newContent;
@@ -41,30 +39,35 @@ function addSmallImage() {
 }
 
 function addBigImage() {
+
+	
 	var newContent = "" + "<div id='imagecontainerBig"+ idBig + "' class='row margin-bottom centertext'>"
 			+ "<div class='col-md-6'>"
 			+ "<div class='main-img thumbnail '>"
-			+ "<button class='test' onclick='addTextArea('leftSide'" + AreaLeftId + ")'>Add text area</button><br>"
-			+ "<input name='file' id='file' type='file'class='inputfile'>"
-			+ "<label for='file'>Add file</label>"
+			+ "<button class='test' onclick='addTextArea('leftSide" + AreaLeftId + "')'>Add text area</button><br>"		
+			+ "<input name='fileLeft" + AreaLeftId + "' id='fileLeft" + AreaLeftId + "' type='file' class='inputfile' onchange='loadFileLeft(event, " + AreaLeftId + ")'>"
+			+ "<label for='fileLeft" + AreaLeftId + "'>Add file</label>"
+			+ "<img class='workimage_big' id='outputLeft" + AreaLeftId + "'/>"
 			+ "</div>"
 			+ "</div>"
 			+ "<div class='col-md-6'>"
 			+ "<div class='row'>"
 			+ "<div class='col-md-12'>"
 			+ "<div class='thumbnail '>"
-			+ "<button class='test' onclick='addTextArea('leftSide'" + AreaLeftId + ")'>Add text area</button><br>"
-			+ "<input name='file' id='file' type='file'class='inputfile'>"
-			+ "<label for='file'>Add file</label>"
+			+ "<button class='test' onclick='addTextArea('leftSide'" + AreaRightId + ")'>Add text area top</button>"
+			+ "<button class='test' onclick='addTextArea('leftSide'" + AreaBottomRightId + ")'>Add text area bottom</button><br>"
+			+ "<input name='fileRight" + AreaRightId + "' id='fileRight" + AreaRightId + "' type='file' class='inputfile' onchange='loadFileRight(event, " + AreaRightId + ")'>"
+			+ "<label for='fileRight" + AreaRightId + "'>Add photo top</label>"
+			+ "<input name='fileBottomRight" + AreaBottomRightId + "' id='fileBottomRight" + AreaBottomRightId + "' type='file' class='inputfile' onchange='loadFileBottomRight(event, " + AreaBottomRightId + ")'>"
+			+ "<label for='fileBottomRight" + AreaBottomRightId + "'>Add photo bottom</label>"				
+			+ "<img class='workimage_small' id='outputRight" + AreaRightId + "'/>"
 			+ "</div>"
 			+ "</div>"
 			+ "</div>"
 			+ "<div class='row'>"
 			+ "<div class='col-md-12'>"
 			+ "<div class='thumbnail '>"
-			+ "<button class='test' onclick='addTextArea('leftSide'" + AreaLeftId + ")'>Add text area</button><br>"
-			+ "<input name='file' id='file' type='file'class='inputfile'>"
-			+ "<label for='file'>Add file</label>"
+			+ "<img class='workimage_small' id='outputBottomRight" + AreaBottomRightId + "'/>"
 			+ "</div>"
 			+ "</div>"
 			+ "</div>"
@@ -78,6 +81,9 @@ function addBigImage() {
 	var oldContent = document.getElementById("placeholder");
 	oldContent.outerHTML = newContent;
 	idBig++
+	AreaLeftId++;
+	AreaRightId++;
+	AreaBottomRightId++;
 }
 
 function addTextArea(leftID){
@@ -92,24 +98,29 @@ function addTextArea(leftID){
 //	4. add remove button
 }
 
-function removeImage(a) {
-	var element = document.getElementById('imagecontainerSmall' + a);
-	console.log(a);
+function removeImage(id) {
+	var element = document.getElementById('imagecontainerSmall' + id);
+	console.log(id);
 	element.parentNode.removeChild(element);
 }
 
-function removeBigImage(b) {
-	var element = document.getElementById('imagecontainerBig' + b);
-	console.log(b);
+function removeBigImage(id) {
+	var element = document.getElementById('imagecontainerBig' + id);
+	console.log(id);
 	element.parentNode.removeChild(element);
 }
 
 function loadFileLeft(event, number) {
     var output = document.getElementById('outputLeft'+number);
     output.src = URL.createObjectURL(event.target.files[0]);
-  };
+};
   
-  function loadFileRight(event, number) {
-	    var output = document.getElementById('outputRight'+number);
-	    output.src = URL.createObjectURL(event.target.files[0]);
-	  };
+function loadFileRight(event, number) {
+	var output = document.getElementById('outputRight'+number);
+	output.src = URL.createObjectURL(event.target.files[0]);
+};
+
+function loadFileBottomRight(event, number) {
+	var output = document.getElementById('outputBottomRight'+number);
+	output.src = URL.createObjectURL(event.target.files[0]);
+};
